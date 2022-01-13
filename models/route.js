76fd83451bd7aa496f6f8b94b3da5a6a2e0cf934
Jairo <jairo.j5180@gmail.com>
2022-01-12 23:46:24 -0600

    route model and migration added

---
+++ models/route.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Route extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Route.init({
+    startLocationId: DataTypes.INTEGER,
+    endLocationId: DataTypes.INTEGER,
+    trainingId: DataTypes.INTEGER,
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Route',
+  });
+  return Route;
+};
