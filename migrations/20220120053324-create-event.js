2dc442646f236ebe37e3085a00b1bd42d9abb386
franzua <edson.andrez@outlook.es>
2022-01-19 23:45:54 -0600

    event model and migration added.

---
+++ migrations/20220120053324-create-event.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('event', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      typeId: {
+        type: Sequelize.INTEGER
+      },
+      typeName: {
+        type: Sequelize.STRING
+      },
+      details: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Events');
+  }
+};
