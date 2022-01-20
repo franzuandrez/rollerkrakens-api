927d0202ac4c642edbda1969dddeca015b018dce
franzua <edson.andrez@outlook.es>
2022-01-19 23:58:59 -0600

    eventMedia belongs to event

--- models/eventmedia.js
+++ models/eventmedia.js
@@ -1,27 +1,27 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class EventMedia extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      this.belongsToMany(models.event, {through: 'event_id', as: 'event'});
-    }
-  };
-  EventMedia.init({
-    path: DataTypes.STRING,
-    mediaType: DataTypes.STRING,
-    name: DataTypes.STRING,
-    eventId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'EventMedia',
-  });
-  return EventMedia;
+    class EventMedia extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            this.belongsTo(models.event, {through: 'eventId', as: 'event'});
+        }
+    };
+    EventMedia.init({
+        path: DataTypes.STRING,
+        mediaType: DataTypes.STRING,
+        name: DataTypes.STRING,
+        eventId: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'EventMedia',
+    });
+    return EventMedia;
 };
