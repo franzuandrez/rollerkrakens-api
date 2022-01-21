f470737902beff2bcc501defb696c0da1f9dd078
franzua <edson.andrez@outlook.es>
2022-01-20 22:45:45 -0600

    foreign key between media_skater and skater

--- migrations/20220113054256-create-media-skater.js
+++ migrations/20220113054256-create-media-skater.js
@@ -1,34 +1,38 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('media_skater', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      skaterId: {
-        type: Sequelize.INTEGER
-      },
-      mediaId: {
-        type: Sequelize.INTEGER,
-        references:{
-          model:'event_media',
-          key:'id'
-        }
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('media_skater');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('media_skater', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            skaterId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'skater',
+                    key: 'id'
+                }
+            },
+            mediaId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'event_media',
+                    key: 'id'
+                }
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('media_skater');
+    }
 };
