f7c9e6b590ccb5784297160b163f62e6b576edc6
franzua <edson.andrez@outlook.es>
2022-01-20 22:52:28 -0600

    foreign key between inventory and skate-part

---
+++ migrations/20220212061121-create-inventory.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('inventory', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            partId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'skate_parts',
+                    key: 'id'
+                }
+            },
+            quantity: {
+                type: Sequelize.INTEGER
+            },
+            factor: {
+                type: Sequelize.INTEGER
+            },
+            lentTo: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('inventory');
+    }
+};
