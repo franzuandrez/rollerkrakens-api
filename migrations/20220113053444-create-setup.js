a68f14bd09196e1c241f83f49569fafbf7a93275
franzua <edson.andrez@outlook.es>
2022-01-20 22:44:24 -0600

    foreign key between setup and skate_parts

--- migrations/20220113053444-create-setup.js
+++ migrations/20220113053444-create-setup.js
@@ -1,30 +1,34 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('setup', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      partId: {
-        type: Sequelize.INTEGER
-      },
-      quantity: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('setup');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('setup', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            partId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'skate_parts',
+                    key: 'id'
+                }
+            },
+            quantity: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('setup');
+    }
 };
