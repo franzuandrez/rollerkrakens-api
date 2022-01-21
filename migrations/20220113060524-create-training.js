dfbb71b304bed3b7d47516b78a3748b74ca39bf1
franzua <edson.andrez@outlook.es>
2022-01-20 22:47:17 -0600

    foreign key between training and training_type, and training and location

--- migrations/20220113060524-create-training.js
+++ migrations/20220113060524-create-training.js
@@ -1,36 +1,44 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('training', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      locationId: {
-        type: Sequelize.INTEGER
-      },
-      startedAt: {
-        type: Sequelize.DATE
-      },
-      endedAt: {
-        type: Sequelize.DATE
-      },
-      typeId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('training');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('training', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            locationId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'location',
+                    key: 'id'
+                }
+            },
+            startedAt: {
+                type: Sequelize.DATE
+            },
+            endedAt: {
+                type: Sequelize.DATE
+            },
+            typeId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'trainingType',
+                    key: 'id'
+                }
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('training');
+    }
 };
