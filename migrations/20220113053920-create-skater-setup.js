8538bb90a17d21d505c30553a4d11c6b9e05684a
franzua <edson.andrez@outlook.es>
2022-01-20 22:39:02 -0600

    foreign key between skater-setup and skater, and skater-setup and setup

--- migrations/20220113053920-create-skater-setup.js
+++ migrations/20220113053920-create-skater-setup.js
@@ -1,30 +1,38 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('skater_setups', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      skaterId: {
-        type: Sequelize.INTEGER
-      },
-      setupId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('skater_setups');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('skater_setups', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            skaterId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'skater',
+                    key: 'id'
+                }
+            },
+            setupId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'setup',
+                    key: 'id'
+                }
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('skater_setups');
+    }
 };
